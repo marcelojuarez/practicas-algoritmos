188738be47e026eca5e954c51c1041c4fd51f9d3
Marcelo Juarez <marcelojuarez@MacBook-Air-de-Marcelo.local>
2024-07-28 01:22:34 -0300

    merge sort implemented

--- java/arraySorter/ArraySorter.java
+++ java/arraySorter/ArraySorter.java
@@ -1,5 +1,7 @@
 package arraySorter;
 
+import java.util.*;
+
 /**
 * Provee métodos para ordenar arreglos de objetos comparables.
 * Los algoritmos de ordenamiento provistos por esta clase son:
@@ -44,11 +46,9 @@ public class ArraySorter {
    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
    */
    public static <T extends Comparable<? super T>> void selectionSort(T[] array) {
-      if (array == null) throw new IllegalArgumentException("array is null, can't sort");
-      //last = indice del ultimo elemento de la parte no ordenada
-      for (int last = array.length - 1; last >= 1; last--) {
-         //largest = posicion del elemento mas grande
-         int largest = indexOfLargest(array, last + 1);
+      if (array == null) throw new IllegalArgumentException("null array");
+      for (int last = array.length-1;last >= 1;last--){
+         int largest = indexOfLargest(array,last+1);
          swap(array, last, largest);
       }
    }
@@ -74,29 +74,27 @@ public class ArraySorter {
    }
 
    private static<T extends Comparable<? super T>> void quickSortRec(Comparable[] array, int begin, int end){
-      if (begin < end){
-         //calculo la particion 
-         int p = partition(array,begin,end);
-         //ordeno parte izquierda
-         quickSortRec(array, begin, p);
-         //ordeno parte derecha         
-         quickSortRec(array, p+1, end);
+      if (begin < end) {
+      int p = partition(array,begin,end);
+      quickSortRec(array,begin, p);
+      quickSortRec(array, p+1, end);
+
       }
    }
    //implementacion de partition
    private static <T extends Comparable<? super T>> int partition(Comparable[] array ,int begin,int end){
       Comparable pivot = array[begin];
-      int i = begin-1;
-      int j = end+1;
-      while (i < j){
-         do j--; while (array[j].compareTo(pivot) > 0);
-         do i++; while (array[i].compareTo(pivot) < 0);
+      int i = begin - 1;
+      int j = end + 1;
+      while (i < j) {
+         do i++ ;while(array[i].compareTo(pivot) < 0);
+         do j-- ;while(array[j].compareTo(pivot) > 0);
          if (i < j) {
             swap(array, i, j);
          }
       }
       return j;
-   }
+    }
    
    
    /**
@@ -105,9 +103,63 @@ public class ArraySorter {
    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
    */
    public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
-      throw new UnsupportedOperationException("TODO: implementar");    
+      mergeSortRe(array, 0, array.length-1);   
    }
 
+   private static <T extends Comparable> void mergeSortRe(T[] array, int begin, int end) {
+      if (begin < end) {
+         int mid = (begin + end) / 2;
+         mergeSortRe(array, begin, mid);
+         mergeSortRe(array, mid+1, end);
+         merge(array, begin, mid, end);
+      }
+   }
+
+   private static <T extends Comparable> void merge(T[] array, int begin, int mid, int end) {
+      int leftLength = mid - begin + 1;
+      int rightLength =  end - mid;
+      
+      //arreglo izquiero y derecho para realizar  merge
+      List<T> arrayLeft = new ArrayList<>(leftLength);
+      List<T> arrayRight = new ArrayList<>(rightLength);
+      
+      //se copian los elementos
+      for (int i = 0;i < leftLength;i++) {
+         arrayLeft.add(array[begin+i]);
+      }
+
+      for (int j = 0 ;j < rightLength;j++) {
+         arrayRight.add(array[mid+j+1]);
+      }
+   
+      //se realiza el join
+      int i = 0,j = 0;
+      int k = begin;
+
+      while(i < leftLength && j < rightLength) {
+         if (arrayLeft.get(i).compareTo(arrayRight.get(j)) <=0) {
+            array[k] = arrayLeft.get(i);
+            i++;
+         }else{
+            array[k] = arrayRight.get(j);
+            j++;
+         }
+         k++;
+      }
+   
+      //copio elementos restantes 
+      while (i < leftLength) {
+         array[k] = arrayLeft.get(i);
+         k++;
+         i++;
+      }
+   
+      while (j < rightLength) {
+         array[k] = arrayRight.get(j);
+         j++;
+         k++;
+      }
+   }
 
    /* (non-Javadoc)
    * Este método intercambia dos posiciones de un arreglo.
